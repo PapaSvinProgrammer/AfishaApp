8e75eb605a1fe78e7136a47a8c197f2dd4b79828
Roman Uraykin <PAIHETITTY@yandex.ru>
2025-02-22 20:03:44 +0300

    Update UI

--- app/src/main/java/com/example/afishaapp/ui/screen/home/HomeScreen.kt
+++ app/src/main/java/com/example/afishaapp/ui/screen/home/HomeScreen.kt
@@ -10,6 +10,7 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.material3.ripple
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
@@ -35,7 +36,7 @@ fun HomeScreen(
     viewModel.getCity()
     viewModel.getDefaultCity()
 
-    CenterAlignedTopAppBar(
+    TopAppBar(
         navigationIcon = {
             Row(
                 verticalAlignment = Alignment.CenterVertically,
